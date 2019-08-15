66051ade1c1d9cc5f9de1c0348f200e5a6d297cd
Avvarus <avvaru79.java@mail.com>
2019-08-15 14:13:04 -0400

    LinkedList

---
+++ src/com/company/LinkedListInJava.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class LinkedListInJava {
+
+    public static void main(String[] args) {
+
+        LinkedListInJava linkedListInJava = new LinkedListInJava();
+
+
+    }
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            data = this.data;
+            next = null;
+        }
+
+    }
+}
